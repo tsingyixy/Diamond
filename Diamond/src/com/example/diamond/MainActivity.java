40fafd27b047a3aa9c2223b06ee4e22d17649654
tsingyixy <tsingyixy@github.com>
2014-02-18 22:06:49 +0800

    add calculate

--- Diamond/src/com/example/diamond/MainActivity.java
+++ Diamond/src/com/example/diamond/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends Activity {
     private Button leftBtn;
     private Button rightBtn;
     private Button transBtn;
+    private boolean isNew;
+    private ShapeDiamond sd;
+    private int[][] diamonds;
+    private int pos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +22,79 @@ public class MainActivity extends Activity {
         rightBtn = (Button)findViewById(R.id.button2);
         transBtn = (Button)findViewById(R.id.button3);
         gv = (GameView)findViewById(R.id.gameView1);
+        isNew = true;
+        diamonds = gv._diamonds;
+        pos = diamonds.length / 2 -1;
         
     }
-
+    public void Calculate(ShapeDiamond sd){
+    	
+    	int[][] shape = sd.GetShape();
+    	if(isNew){
+        	for (int i = 0; i < shape.length; i++) {
+    			for (int j = 0; j < shape[i].length; j++) {
+    				diamonds[pos+i][j] |= shape[i][j];
+    			}
+    		}
+        	pos++;
+        	isNew = false;
+    	}
+    	else{
+    		for (int i = 0; i < shape.length; i++) {
+    			for (int j = 0; j < shape[i].length; j++) {
+    				diamonds[pos+i][j] = 0;
+    			}
+    		}
+        	pos++;
+        	for (int i = 0; i < shape.length; i++) {
+    			for (int j = 0; j < shape[i].length; j++) {
+    				diamonds[pos+i][j] |= shape[i][j];
+    			}
+    		}
+    	}
+    	
+    	
+    }
+    private void Run(){
+    	while(true){
+    		if(isNew){
+    			sd = new ShapeDiamond(0);
+    		}
+    		Calculate(sd);   		
+    	}
+    }
     class ShapeDiamond {
-    	private int 
+    	private int[][] shape;
+    	//public static boolean isNew = false;
+    	public ShapeDiamond(int flag){
+    		//shape = new int[2][4];
+    		Generate(flag);
+    	}
+    	public void Generate(int flag){
+    		switch (flag) {
+			case 0:
+				shape = new int[][]{{0,1,1,0},{0,1,1,0}};
+				break;
+			case 1:
+				shape = new int[][]{{0,0,1,0},{1,1,1,0}};
+				break;
+			default:
+				break;
+			}
+    	}
+    	public int[][] GetShape(){
+    		return shape;
+    	}
+    	public void Transpos(){
+    		int[][] temp = {{0,0},{0,0},{0,0},{0,0}};
+    		for (int i = 0; i < temp.length; i++) {
+				for (int j = 0; j < temp[i].length; j++) {
+					temp[i][j] = shape[j][i];
+				}
+			}
+    		shape = temp;
+    	}
+    	
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
