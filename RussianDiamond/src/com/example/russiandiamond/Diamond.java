b64c9e1b8e55abcff517e6daced63f80057a8a31
Huiquan Tan <hutan@sanramon.(none)>
2014-02-25 17:02:03 +0800

    new framework of game

---
+++ RussianDiamond/src/com/example/russiandiamond/Diamond.java
@@ -0,0 +1,26 @@
+package com.example.russiandiamond;
+
+public class Diamond {
+
+	public Diamond() {
+		// TODO Auto-generated constructor stub
+	}
+	public int LeftMove(){ 
+		return 0;
+		
+	}
+	public int RightMove(){
+		return 0;
+	}
+    public int Transformation(){
+    	return 0;
+    }
+    public int Accerlate(){           //down more quickly
+    	return 0;
+    }
+}
+class Sreen{
+	public int Display(){
+		return 0;
+	}
+}
---
+++ RussianDiamond/src/com/example/russiandiamond/DiamondView.java
@@ -0,0 +1,44 @@
+package com.example.russiandiamond;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class DiamondView extends SurfaceView implements Callback {
+
+	public DiamondView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DiamondView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DiamondView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
