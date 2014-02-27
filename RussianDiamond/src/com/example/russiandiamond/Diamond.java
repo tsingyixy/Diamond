e68302accbb24ada509d6e8622d79e8314a43617
Huiquan Tan <hutan@sanramon.(none)>
2014-02-27 17:19:21 +0800

    add class world,screen

--- RussianDiamond/src/com/example/russiandiamond/Diamond.java
+++ RussianDiamond/src/com/example/russiandiamond/Diamond.java
@@ -1,15 +1,31 @@
 package com.example.russiandiamond;
 
-public class Diamond {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Diamond {
+    public int x;
+    public int y;
 	public Diamond() {
-		// TODO Auto-generated constructor stub
+        this.x = 0;
+        this.y = 0;
 	}
+    public Diamond(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+class ShapeDiamond{
+    public List<Diamond> diamonds;
 	public int LeftMove(){ 
+		for(Diamond d :diamonds)
+			d.x -= 1;
 		return 0;
 		
 	}
 	public int RightMove(){
+		for(Diamond d :diamonds)
+			d.x += 1;
 		return 0;
 	}
     public int Transformation(){
@@ -18,9 +34,53 @@ public class Diamond {
     public int Accerlate(){           //down more quickly
     	return 0;
     }
+    public void Down(int step){
+    	for(Diamond d : diamonds)
+    		d.y += step;
+    }
+    public void update(){
+    	this.Down(1);
+    }
 }
-class Sreen{
-	public int Display(){
-		return 0;
+class World{
+	public List<Diamond> diamonds;
+	private ShapeDiamond shape;
+	private int sum[];
+	//private boolean isNew;
+	public World(){
+		this.diamonds = new ArrayList<Diamond>();
+		shape = new ShapeDiamond();
+		sum = new int[20];
+		//isNew = false;
+	}
+	public void update(){
+		for(Diamond d :shape.diamonds){
+			d.y += 1;
+			if (diamonds.contains(d))
+				{
+                    d.y -= 1;				    
+				    diamonds.addAll(shape.diamonds);
+				    shape = new ShapeDiamond();
+				    //isNew = true;
+				    break;
+				}
+			else
+				d.y -= 1;
+		}
+		//if (!isNew)
+		    shape.update();
+
+        for (Diamond d:diamonds) {
+			sum[ d.y ] ++;
+		}
+        for (int i = 0; i < sum.length; i++) {
+			if (sum[i] == 16)
+				for(int j = 0 ; j < diamonds.size() ; ++j)
+					if(diamonds.get(j).y == i)
+						diamonds.remove(j);
+					else if(diamonds.get(j).y < i)
+						diamonds.get(j).y += 1;
+		}
 	}
 }
+
---
+++ RussianDiamond/src/com/example/russiandiamond/Screen.java
@@ -0,0 +1,24 @@
+package com.example.russiandiamond;
+
+public class Screen {
+	public int update(){
+		return 0;
+	}
+	public int present(){
+		return 0;
+	}
+}
+class LoadingScreen extends Screen{
+	public LoadingScreen(){
+		super();
+	}
+}
+class MainScreen extends Screen{
+	
+}
+class GameScreen extends Screen{
+	
+}
+class EndScreen extends Screen{
+	
+}
